63865820753995a3b6c2ea7f552ebaea8acb4d63
bob chase <bob.chase@gmail.com>
2020-12-26 07:40:46 -0500

    add simple query runner

---
+++ aiomysql/cli.py
@@ -0,0 +1,27 @@
+import asyncio
+
+import click
+
+from aiomysql.connection import MysqlConnection
+
+
+@click.command()
+@click.argument("host")
+@click.argument("user")
+@click.option("--password", default="")
+@click.option("--database")
+@click.argument("query")
+def select(host, user, password, database, query):
+    """simple query runner"""
+
+    async def _select():
+        con = await MysqlConnection().connect(host, user, password, database)
+        cols, rows = await con.execute(query)
+        for row in rows:
+            print(dict(zip(cols, row)))
+
+    asyncio.run(_select())
+
+
+if __name__ == "__main__":
+    asyncio.run(select())
